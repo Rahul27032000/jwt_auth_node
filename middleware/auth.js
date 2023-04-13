d132ea60e87ed683124a61349a1badbd78d7dcb3
Shinde Rahul <shinde2732000@gmail.com>
2023-04-14 02:21:05 +0530

    added authentication feature

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,18 @@
-// const { ACCESS_TOKEN_SECRET } = require("../config/config");
-// const User = require("../models/user")
-// const jwt = require("jsonwebtoken");
+const { ACCESS_TOKEN_SECRET } = require("../config/config");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const authentication = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) return res.status(401).send("unauthorized");
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decoded.id);
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports = authentication;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const { port, db } = require("./config/config");
 const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+const authentication = require("./middleware/auth");
 const app = express();
 db();
 
 app.use(express.json());
 
 app.use("", authRouter);
+app.use("/post", authentication, postRouter);
 
 app.listen(port, console.log(port));
